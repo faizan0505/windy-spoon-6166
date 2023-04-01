e379e5f40e4c0af8f0199d8ececf9c9187a384e1
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-04-02 03:48:39 +0530

    Dark theme fixed and Homepage finished

--- Nishtha/scripts/index.js
+++ Nishtha/scripts/index.js
@@ -2,15 +2,47 @@
 function myTheme(){
     const toggle= document.getElementById("darkmode-Toggle")
     const body= document.getElementById("main")
+    const h= document.getElementsByTagName("h5")
+    const p= document.getElementsByTagName("p")
+    const l= document.getElementsByTagName("li")
+    const u= document.getElementsByTagName("ul")
     const second_row= document.getElementById("second_row")
-    console.log("hi")
+    const sixth_row= document.getElementById("sixth_row");
+    const f_row= document.getElementById("Fourth_row")
     if (toggle.checked == true){
-        console.log("hi")
         body.style.background = "#4D4D4D";
         second_row.style.background = "#4D4D4D";
+        sixth_row.style.background = "#4D4D4D";
+        f_row.style.background = "#4D4D4D";
+        for(let i=0;i<h.length;i++){
+            h[i].style.color="#FFFFFF";
+        }
+        for(let i=0;i<p.length;i++){
+            p[i].style.color="#FFFFFF";
+        }
+        for(let i=0;i<l.length;i++){
+            l[i].style.color="#FFFFFF";
+        }
+        for(let i=0;i<u.length;i++){
+            u[i].style.color="#FFFFFF";
+        }
       }else{
         body.style.background = "#FFFFFF";
         second_row.style.background = "#F9F9F9";
+        sixth_row.style.background = "#F9F9F9";
+        f_row.style.background = "#F9F9F9";
+        for(let i=0;i<h.length;i++){
+            h[i].style.color="#4D4D4D";
+        }
+        for(let i=0;i<p.length;i++){
+            p[i].style.color="#4D4D4D";
+        }
+        for(let i=0;i<l.length;i++){
+            l[i].style.color="#4D4D4D";
+        }
+        for(let i=0;i<u.length;i++){
+            u[i].style.color="#4D4D4D";
+        }
 
       }
 }
