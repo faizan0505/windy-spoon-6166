2065aeebda3bc0e963f003c7b099d4ef9d957167
unknown <md.faizan9t9@gmail.com>
2023-04-02 13:53:01 +0530

    nav js

---
+++ Nishtha/scripts/nav-foot.js
@@ -0,0 +1,20 @@
+document.querySelector("#logo_img").addEventListener("click",()=>{
+    window.location.href="index.html"
+})
+
+
+document.querySelector("#explory").addEventListener("click",()=>{
+    window.location.href="../faizan/frontend/explore.html"
+})
+
+document.querySelector("#working").addEventListener("click",()=>{
+    window.location.href="homepage.html"
+})
+
+document.querySelector("#sign_in_div").addEventListener("click",()=>{
+    window.location.href="../faizan/frontend/login.html"
+})
+
+document.querySelector("#sign_up_div").addEventListener("click",()=>{
+    window.location.href="../faizan/frontend/signup.html"
+})
